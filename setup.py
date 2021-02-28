8a57afa5db30740fc737f3c3fa4f4240d501dcdd
Sch <junhg0211@gmail.com>
2021-02-28 13:46:39 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from cx_Freeze import setup, Executable
 
 build_exe_options = {'packages': ['keyboard', 'json']}
 
-setup(name="Shtelian Keyboard",
+setup(name="Stelian Keyboard",
       version="1.0",
-      description="Shtelian Keyboard made by Sch",
+      description="Stelian Keyboard made by Sch",
       options={"build_exe": build_exe_options},
-      executables=[Executable("__main__.py", target_name='ShtelianKeyboard')])
+      executables=[Executable("__main__.py", target_name='StelianKeyboard')])
